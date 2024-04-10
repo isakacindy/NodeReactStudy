9e5c5c856825643965a3586249c0419bc042c710
cindy <cindy@cindyui-MacBookPro.local>
2024-04-10 16:45:59 +0900

    Lecture #8

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://isakacindy:rhedmsqortjd6!3@boilerplate.uagme0l.mongodb.net/')
@@ -9,6 +17,23 @@ mongoose.connect('mongodb+srv://isakacindy:rhedmsqortjd6!3@boilerplate.uagme0l.m
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', async (req, res) => {
+  //회원가입시 필요 정보를 client에서 가져오면
+  //데이터베이스에 삽입한다
+
+  //body parser를 통해 body에 담긴 정보를 가져온다
+  const user = new User(req.body)
+
+  //mongoDB 메서드, user모델에 저장
+  const result = await user.save().then(()=>{
+    res.status(200).json({
+      success: true
+    })
+  }).catch((err)=>{
+    res.json({ success: false, err })
+  })
 })
 
 app.listen(port, () => {
