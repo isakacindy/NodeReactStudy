2c69cfcd86ea127c4e750cff23174d31cba2c970
cindy <cindy@cindyui-MacBookPro.local>
2024-04-10 19:18:12 +0900

    Lecture #9

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://isakacindy:rhedmsqortjd6!3@boilerplate.uagme0l.mongodb.net/')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err))
 
